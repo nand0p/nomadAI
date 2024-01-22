0cece232702b0293979821df034e01ff47bb3a1e
nando <nando@hex7.com>
2024-01-23 00:35:55 +0100

    train

---
+++ train/docker_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+VERSION=v001
+
+for i in $(find docker -type d); do
+  echo "build docker $i:${VERSION}";
+  echo
+  echo "run docker $i:${VERSION}";
+  echo
+  echo "test docker $i:${VERSION}";
+  echo
+done
+
+echo namespace containers appropriately for nomad job definitions
+
+echo after container is built and tests pass
+
+echo nomad job definition is updated with new tag version
+
+echo nomad job definition is executed 
+
+echo assert container runs in cluster
